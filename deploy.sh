2daf089dc2724c1350d6caa55d15154831cb1d7f
gians <giansluca@gmail.com>
2020-11-02 23:46:52 +0100

    commit

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 docker build -t gianlucamori/multi-client -f ./client/Dockerfile ./client
 docker build -t gianlucamori/multi-api -f ./api/Dockerfile ./api
 docker build -t gianlucamori/multi-worker -f ./worker/Dockerfile ./worker
-
 docker push gianlucamori/multi-client
 docker push gianlucamori/multi-api
 docker push gianlucamori/multi-worker
-
-kubectl apply -f k8s/
