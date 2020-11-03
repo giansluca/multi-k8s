08b3746cbfc26016fee1758b02ef2c8a33316f2c
gians <giansluca@gmail.com>
2020-11-03 22:59:57 +0100

    commit

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
-docker build -t gianlucamori/multi-client -f ./client/Dockerfile ./client
-docker build -t gianlucamori/multi-api -f ./api/Dockerfile ./api
-docker build -t gianlucamori/multi-worker -f ./worker/Dockerfile ./worker
-docker push gianlucamori/multi-client
-docker push gianlucamori/multi-api
-docker push gianlucamori/multi-worker
+docker build -t gianlucamori/multi-client:latest -t gianlucamori/multi-client:$GIT_SHA  -f ./client/Dockerfile ./client
+docker build -t gianlucamori/multi-api:latest -t gianlucamoti/multi-api:$GIT_SHA -f ./api/Dockerfile ./api
+docker build -t gianlucamori/multi-worker:latest -t gianlucamori/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+
+docker push gianlucamori/multi-client:latest
+docker push gianlucamori/multi-api:latest
+docker push gianlucamori/multi-worker:latest
+
+docker push gianlucamori/multi-client:$GIT_SHA
+docker push gianlucamori/multi-api:$GIT_SHA
+docker push gianlucamori/multi-worker:latest:$GIT_SHA
+
+kubectl apply -f k8s/
+kubectl set image deployment/client-deployment client=gianlucamori/multi-client:$GIT_SHA
+kubectl set image deployment/api-deployment api=gianlucamori/multi-api:$GIT_SHA
+kubectl set image deployment/worker-deployment worker=gianlucamori/multi-worker:$GIT_SHA
