d462bef4b1b1166c6f31413047fa2ee54cca4d6d
gians <giansluca@gmail.com>
2020-11-04 10:05:07 +0100

    commit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker build -t gianlucamori/multi-client:latest -t gianlucamori/multi-client:$GIT_SHA  -f ./client/Dockerfile ./client
-docker build -t gianlucamori/multi-api:latest -t gianlucamoti/multi-api:$GIT_SHA -f ./api/Dockerfile ./api
+docker build -t gianlucamori/multi-api:latest -t gianlucamori/multi-api:$GIT_SHA -f ./api/Dockerfile ./api
 docker build -t gianlucamori/multi-worker:latest -t gianlucamori/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
 
 docker push gianlucamori/multi-client:latest
@@ -8,7 +8,7 @@ docker push gianlucamori/multi-worker:latest
 
 docker push gianlucamori/multi-client:$GIT_SHA
 docker push gianlucamori/multi-api:$GIT_SHA
-docker push gianlucamori/multi-worker:latest:$GIT_SHA
+docker push gianlucamori/multi-worker:$GIT_SHA
 
 # kubectl apply -f k8s/
 # kubectl set image deployment/client-deployment client=gianlucamori/multi-client:$GIT_SHA
